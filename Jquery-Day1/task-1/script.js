a492ac33089b81540df35aeb75afd84409185c58
Mohamed Abdellhay <mohamedabdellhay1@gmail.com>
2025-08-03 14:16:02 +0300

    update task-1

--- Jquery-Day1/task-1/script.js
+++ Jquery-Day1/task-1/script.js
@@ -2,8 +2,22 @@ $(function () {
   console.log("start dev using jQuery");
 
   $(".btn").click(function (index, element) {
+    const $contents = $(".content-container > div");
+    const buttonIndex = $(".btn").index(this);
     $(".btn").removeClass("highlight");
+
+    console.log("btn index", buttonIndex);
+
     $(this).addClass("highlight");
-    $(".content").text($(this).data("content"));
+    // $(".content-container > div").each(function (index, element) {
+    //   $(element).addClass("hidden");
+    //   if (index === buttonIndex) $(element).removeClass("hidden");
+    // });
+
+    // use fade in to
+    $contents.stop(true, true).fadeOut(200, function () {
+      $contents.addClass("hidden");
+      $contents.eq(buttonIndex).removeClass("hidden").hide().fadeIn(400);
+    });
   });
 });
