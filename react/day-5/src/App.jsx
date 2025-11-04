17a0dc96a4e86e7ca1111b18a8613b51d38ffb86
Mohamed Abdellhay <mohamedabdellhay1@gmail.com>
2025-11-04 21:34:23 +0200

    add react day-5 task

---
+++ react/day-5/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Register from "./Register";
+
+function App() {
+  return (
+    <div className="max-w-3xl m-auto py-20">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/day-5/src/Register.jsx
@@ -0,0 +1,204 @@
+import { useForm } from "react-hook-form";
+
+export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const saveForm = (e) => {
+    console.log(e);
+  };
+  return (
+    <form onSubmit={handleSubmit(saveForm)}>
+      <div className="space-y-12 bg-[#10182700]">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base/7 font-semibold text-gray-900">Register</h2>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            {/* first name */}
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="firstName"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                first name
+              </label>
+              <div className="mt-2">
+                <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                  {/* <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">
+                    workcation.com/
+                  </div> */}
+                  <input
+                    id="firstName"
+                    {...register("firstName", {
+                      required: true,
+                      minLength: {
+                        value: 6,
+                        message: "First name is too short",
+                      },
+                      maxLength: {
+                        value: 20,
+                        message: "First Name At least 20 char",
+                      },
+                    })}
+                    type="text"
+                    placeholder="abdellhay"
+                    className="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                  />
+                </div>
+                {errors.firstName && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.firstName.message}
+                  </p>
+                )}
+              </div>
+            </div>
+            {/* last name */}
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="lastName"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                last name
+              </label>
+              <div className="mt-2">
+                <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                  <input
+                    id="lastName"
+                    {...register("lastName", {
+                      required: true,
+                      minLength: {
+                        value: 6,
+                        message: "Last name is too short",
+                      },
+                      maxLength: {
+                        value: 20,
+                        message: "Last name is too long",
+                      },
+                    })}
+                    type="text"
+                    placeholder="mohamed"
+                    className="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                  />
+                </div>
+                {errors.lastName && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {errors.lastName.message}
+                  </p>
+                )}
+              </div>
+            </div>
+            {/* email */}
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  {...register("email", {
+                    required: true,
+                    pattern: {
+                      value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                      message: "Email Not Valid",
+                    },
+                  })}
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+              {errors.email && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="border-b border-gray-900/10 pb-12">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="password"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  {...register("password", {
+                    required: "Password is required",
+                    minLength: {
+                      value: 8,
+                      message: "Password must be at least 8 characters",
+                    },
+                    pattern: {
+                      value:
+                        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+                      message:
+                        "Password must include uppercase, lowercase, number, and special character",
+                    },
+                  })}
+                  type="password"
+                  autoComplete="false"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+              {errors.password && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="confirmPassword"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                confirm password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="confirmPassword"
+                  {...register("confirmPassword", {
+                    required: "Please confirm your password",
+                    validate: (value) =>
+                      value === watch("password") || "Passwords do not match",
+                  })}
+                  type="password"
+                  autoComplete="false"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+              {errors.confirmPassword && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.confirmPassword.message}
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" className="text-sm/6 font-semibold text-gray-900">
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
+}
